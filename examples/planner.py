57816e1a5e64de9df4b3929666f3d5b453e3b939
Gleb Karpushkin <rugleb@gmail.com>
2020-01-10 09:59:03 +0300

    created examples folder

---
+++ examples/planner.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+from pfm import Space, PotentialFieldMethod
+
+DIR = Path(__file__).parent
+
+
+def main() -> None:
+    input_path = DIR.joinpath("space.json")
+    space = Space.form_file(input_path)
+    algorithm = PotentialFieldMethod()
+    plan = algorithm.solve(space)
+    output_path = DIR.joinpath("solution.json")
+    plan.dump(output_path)
+
+
+if __name__ == "__main__":
+    main()
